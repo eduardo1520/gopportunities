6afffe27361b57b5913306d51f5395cec4780360
Eduardo Miranda <emiranda.dev@gmail.com>
2024-07-26 16:34:19 -0300

    ✨ Nova funcionalidade adicionando Logger customizado.

--- main.go
+++ main.go
@@ -1,8 +1,22 @@
 package main
 
-import "github.com/eduardo1520/gopportunities/router"
+import (
+	"github.com/eduardo1520/gopportunities/config"
+	"github.com/eduardo1520/gopportunities/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main")
+	// Initialize Configs
+	if err := config.Init(); err != nil {
+		logger.Errorf("config initialization error: %v", err)
+		return
+	}
+
 	// Initialize Router
 	router.Initialize()
 }
